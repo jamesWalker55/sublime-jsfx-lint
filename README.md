33764740c341af78c801fbbfb9ccc91c7c485b20
James Walker <james.chunho@gmail.com>
2026-03-11 23:34:03 +0800

    first implementation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sublime-jsfx-lint
 
 This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides an interface to [jsfx-lint](https://github.com/Souk21/jsfx-lint). It will be used with files that have the “source.jesusonic” syntax (See [ReaSyntax](https://github.com/Breeder/ReaSyntax)).
+
+The ID name of this linter is `"jsfx-lint"`.
--- linter.py
+++ linter.py
@@ -1,10 +1,11 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+import re
+from SublimeLinter.lint import Linter
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
-    defaults = {
-        'selector': 'source.python'
-    }
+class JSFXLint(Linter):
+    cmd = ("jsfx-lint", "-")
+    name = "jsfx-lint"
+    regex = r"""^(?P<error_type>warning|error):\s+(?P<message>.*)$\n\s*-*>\s*(?P<path>[^:]*):(?P<line>\d+):(?P<col>\d+)\s+\((?P<code>[^)]*)\)$"""
+    multiline = True
+    re_flags = re.MULTILINE | re.UNICODE
+    defaults = {"selector": "source.jesusonic"}
